49ecb5e5f70d33423ba338b8fdc16d94c33287ef
Michael McCallum <michael@StickyCode.net>
2012-01-19 22:21:33 +1300

    In many cases its useful to have the owner of a target, for example the owner of a field or method target is the class it has been found on - the concrete type not where it may be declared

--- src/main/java/net/stickycode/coercion/target/CoercionTargets.java
+++ src/main/java/net/stickycode/coercion/target/CoercionTargets.java
@@ -3,6 +3,7 @@ package net.stickycode.coercion.target;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
@@ -11,35 +12,39 @@ import net.stickycode.coercion.CoercionTarget;
 public class CoercionTargets {
 
   public static CoercionTarget find(Class<?> type) {
-    return find(type, type);
+    return find(type, type, type);
   }
 
-  private static CoercionTarget find(Class<?> type, AnnotatedElement element) {
+  private static CoercionTarget find(Class<?> type, AnnotatedElement element, Class<?> owner) {
     if (type.isArray())
-      return new ArrayCoercionTarget(type);
+      return new ArrayCoercionTarget(type, owner);
 
-    return new PrimitiveResolvingCoercionTarget(type, element);
+    return new PrimitiveResolvingCoercionTarget(type, element, owner);
   }
 
   public static CoercionTarget find(Type genericType) {
-    return find(genericType, null);
+    return find(genericType, null, null);
   }
 
-  private static CoercionTarget find(Type genericType, AnnotatedElement element) {
+  private static CoercionTarget find(Type genericType, AnnotatedElement element, Class<?> owner) {
     if (genericType instanceof Class)
-      return find((Class<?>) genericType, element);
+      return find((Class<?>) genericType, element, owner);
 
     if (genericType instanceof ParameterizedType)
-      return new ParameterizedCoercionTarget((ParameterizedType) genericType, element);
+      return new ParameterizedCoercionTarget((ParameterizedType) genericType, element, owner);
 
     if (genericType instanceof GenericArrayType)
       return new ParameterizedArrayCoercionTarget((GenericArrayType)genericType);
     
-    throw new RuntimeException("Dont' know how to deal with generic type " + genericType);
+    throw new CoercionTargetsDoesNotRecogniseTypeException(genericType);
   }
 
   public static CoercionTarget find(Field f) {
-    return find(f.getGenericType(), f);
+    return find(f.getGenericType(), f, f.getDeclaringClass());
+  }
+  
+  public static CoercionTarget find(Method m) {
+    return find(m.getGenericReturnType(), m, m.getDeclaringClass());
   }
 
 }
